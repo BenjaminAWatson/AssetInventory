b14f00b6a2d530918052c4574406ecca3e6ad230
benjamin.watson <benjamin.watson@USC-W-H0658H2-A>
2020-07-02 11:14:20 -0500

    added a comment

--- CGI.Asset.Inventory.DTO/ClientSiteDTO.cs
+++ CGI.Asset.Inventory.DTO/ClientSiteDTO.cs
@@ -6,6 +6,7 @@ namespace CGI.Asset.Inventory.DTO
 {
     public class ClientSiteDTO
     {
+        //this is a comment
         public int ClientSiteKey { get; set; }
         public string ClientSiteName { get; set; }
     }
